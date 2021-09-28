bfb0c47ed7b9bfc14a2d1003a8cefda4f10c0d13
7 <itismymessagebox@mail.ru>
2021-09-28 10:07:07 +0500

    add home work code

--- ProjectForLesson/Program.cs
+++ ProjectForLesson/Program.cs
@@ -6,7 +6,46 @@ namespace ProjectForLesson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            //для 2 задания
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            Console.WriteLine("Задание №1");
+            for (int i = 20; i <= 70; i += 4)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Задание №2");
+            for (int j = 10; j < 16; j++)
+            {
+                Console.WriteLine(letters[j]);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание №3");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("###");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание №4");
+            for (int j = 0; j <= 100; j++)
+            {
+                if (j % 10 == 0)
+                {
+                    Console.Write(j + " ");
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Задание №5");
+            for (int i = 4, j = 40; i - j != 40; i++, j--)
+            {
+                Console.WriteLine(i + " " + j);
+            }
         }
     }
 }
