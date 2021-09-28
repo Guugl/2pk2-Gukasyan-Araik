32e1e19673098e4913937451b16f85f89880b2a1
7 <itismymessagebox@mail.ru>
2021-09-28 08:49:29 +0500

    Добавьте файлы проекта.

---
+++ ProjectForLesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectForLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
