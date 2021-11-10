8536f591e6905a6518398e5bbbc892c323541c7e
7 <itismymessagebox@mail.ru>
2021-11-10 18:12:34 +0500

    pz_11)

---
+++ pz_11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace pz_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Console.ReadLine().Split(" ").Max());
+
+        }
+    }
+}
