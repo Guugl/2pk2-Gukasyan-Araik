d054bb34c902fe830a0c6adf29452df97acc70ee
7 <itismymessagebox@mail.ru>
2021-11-23 18:12:01 +0500

    pz_14

---
+++ pz_12/Program.cs
@@ -0,0 +1,53 @@
+﻿using Org.BouncyCastle.Utilities;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace pz_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = Console.ReadLine();
+            string[] aa = a.Split(". ");
+            string[][] aaa = new string[aa.Length][];
+            int[] l = new int[aa.Length];
+
+            for (int i = 0; i < aa.Length; i++)
+            {
+                aaa[i] = aa[i].Split(" ");
+                l[i] = aaa[i].Length;
+            }
+
+            Array.Sort(aaa);
+
+            foreach (var item in aaa)
+            {
+                Arrays.sort(ite, (x, y)->x.split(" ").length - y.split(" ").length);
+            }
+
+            /*for (int i = 0; i < aaa.Length; i++)
+            {
+                for (int i1 = 0; i1 < aaa.Length; i1++)
+                {
+                    string[] item = aaa[i1];
+                    if (item.Length == i)
+                    {
+                        foreach (var it in item)
+                        {
+                            Console.Write(it + " ");
+                        }
+                    }
+                }
+            }
+
+            foreach (var it in aaa[aaa.Length-1])
+            {
+                Console.Write(it + " ");
+            }*/
+
+        }
+    }
+}
---
+++ pz_14/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace pz_14
+{
+    class Program
+    {
+
+        const string PATH = @"C:\Users\7\Desktop\res.txt";
+
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream(PATH, FileMode.Open, FileAccess.ReadWrite);
+            StreamReader reader = new StreamReader(file);
+            string str = reader.ReadToEnd();
+            
+            List<string> strList = new List<string>();
+            strList.AddRange(str.Split("\n"));
+
+            DateTime dtVar = DateTime.Now;
+
+            for (int i = 0; i < strList.Count; i++)
+            {
+                string item = strList[i];
+
+                if (i == 0)
+                {
+                    dtVar = DateTime.Parse(item.Split(" ")[0]);
+                }
+                else
+                {
+                    if (dtVar > DateTime.Parse(item.Split(" ")[0]))
+                    {
+                        dtVar = DateTime.Parse(item.Split(" ")[0]);
+                    }
+                }
+            }
+
+            string date;
+
+            if (dtVar.Month > 9)
+            {
+                date = dtVar.Year + "-" + dtVar.Month + "-" + dtVar.Day;
+            } 
+            else
+            {
+                date = dtVar.Year + "-" + "0" + dtVar.Month + "-" + dtVar.Day;
+            }
+
+
+            str = str.Substring(str.IndexOf(date) + date.Length + 1, str.Length - (str.IndexOf(date) + date.Length + 1));
+            var lol = str.Split("\n");
+            Console.WriteLine(lol[0]);
+
+        }
+    }
+}
